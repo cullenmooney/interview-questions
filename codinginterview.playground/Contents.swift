064a972c0ef7b8122073d46a808a5ba57dc4830f
Cullen Mooney <cullen@protonmail.com>
2018-03-14 14:31:36 -0500

    question 3 trickery

--- codinginterview.playground/Contents.swift
+++ codinginterview.playground/Contents.swift
@@ -19,7 +19,7 @@ func sumEqualsK(sampleArray: [Int], k: Int) -> Bool {
 
 sumEqualsK(sampleArray: [10, 15, 3, 7], k: 17)
 
-// Coding Question @ 2
+// Coding Question # 2
 // Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i. Solve it without using division and in O(n) time.
 // For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6]
 
@@ -37,4 +37,28 @@ func productOfNumbers(array: [Int]) -> [Int] {
 }
 
 productOfNumbers(array: [1, 2, 3, 4, 5])
+
+// Coding Question # 3
+//Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+//
+//For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+//You can modify the input array in-place.
+
+func findMissingInt(array: [Int]) {
+    var value: Int = 0
+    var newArr: [Int] = array.sorted()
+    for i in 0..<newArr.count {
+        for j in 1..<newArr.count {
+            var difference = newArr[j] - newArr[i]
+            if difference > 1 {
+                value = difference
+            }
+        }
+    }
+    print(value)
+    
+}
+
+findMissingInt(array: [3, 4, 0, 1])
+
 
